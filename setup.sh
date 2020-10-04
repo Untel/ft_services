fb2f68e044e436b8f8033a1cb19a2bc11418d5ff
Adrien Fernandes <untel@mbp-de-adrien.home>
2020-10-04 15:32:48 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-if [[ $1 == "re" ]]
+if [ $1 = "re" ]
 then
     echo "Cleaning..."
     kubectl		delete all --all
@@ -10,16 +10,18 @@ srcs=./srcs
 services=(nginx)
 
 echo "Linking Minikube daemon to Docker daemon"
-minikube start --vm-driver=docker -p minikube docker-env
 eval $(minikube -p minikube docker-env)
+minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
 
 echo "Montage de MetalLB"
 kubectl apply -f srcs/metallb/namespace.yaml
 kubectl apply -f srcs/metallb/metallb.yaml
 kubectl apply -f srcs/metallb/deploy.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#minikube addons enable ingress
+#kubectl apply -f ingress.yaml
 
-for service in "${services[@]}"
+for service in ${services[@]}
 do
 	echo "Création de l'image $service"
 	docker build -t $service-img $srcs/$service
