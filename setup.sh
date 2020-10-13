90172fc7f056b4ad594da6ae69b658690a55a7e2
Adrien Fernandes <adrien.dasilvafernandes@gmail.com>
2020-10-13 14:26:14 +0200

    Pending

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
-if [ $1 = "re" ]
+#!/bin/sh
+
+source ./shortcuts.sh
+
+if [[ $1 == "re" ]]
 then
     echo "Cleaning..."
-    kubectl		delete all --all
-    docker      system prune -a
+    # kubectl		delete all --all
+    # docker      system prune -a
     minikube    delete
 fi
 
 srcs=./srcs
-services=(nginx)
+services=(nginx mysql)
 
+
+# export HTTP_PROXY=http://192.168.99.124:80
+# export HTTPS_PROXY=https://ft_service:443
+# export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.99.0/24,192.168.39.0/24
+
+echo "Starting minikube"
+minikube start --vm-driver=docker --extra-config=apiserver.service-node-port-range=1-35000
 echo "Linking Minikube daemon to Docker daemon"
 eval $(minikube -p minikube docker-env)
-minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+minikube addons enable ingress
 
 echo "Montage de MetalLB"
 kubectl apply -f srcs/metallb/namespace.yaml
@@ -26,6 +37,8 @@ do
 	echo "Création de l'image $service"
 	docker build -t $service-img $srcs/$service
 
-	echo "Deploiement du service $service"
+	echo "Deploiement du service $service on path $srcs/$service/deploy.yaml"
     kubectl apply -f $srcs/$service/deploy.yaml
 done
+
+minikube dashboard &
---
+++ srcs/mysql/mysql.sh
@@ -0,0 +1,7 @@
+echo "Reinstalling files"
+
+mkdir /run/mysqld
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+echo "RUNNING MYSQL!"
+tail -f /dev/null
